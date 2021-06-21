c3b2e6f3e94be814c17daff17a95ece8173aa05b
loganvaz <63812529+loganvaz@users.noreply.github.com>
2021-06-21 11:32:29 -0500

    Add files via upload
    the clean model now there instead, easier to read but still fairly messy

--- efficientEmotionDetectionWithKeras.py
+++ efficientEmotionDetectionWithKeras.py
@@ -1,5 +1,4 @@
 #note: validation accuracy is similar to unaltered, increase model complexity (also, train for a while overnight)
-#data from https://www.kaggle.com/astraszab/facial-expression-dataset-image-folders-fer2013
 
 
 #so I can load the datasets
@@ -105,9 +104,9 @@ from tensorflow.keras import regularizers
 justTestModel = True
 
 #final model
-#GOT test_accuracy OF 68.93% and test F1 of .6924
+#GOT test_accuracy OF 68.93% and test F1 of .6878
 #stored in provenModel folder (do not alter below code or won't be able to load those weights)
-
+"""
 def initializeModel(inputShape, outputDimension):
     #caution against MaxPooling for small images
     #if still overfitting, chagne Conv2Ds to 8, 16, 32
@@ -211,6 +210,58 @@ def initializeModel(inputShape, outputDimension):
     model = Model(inputs = X_input, outputs = X, name="myCNN")
     return model
 
+"""
+def initializeModel(inputShape, outputDimension):
+
+    X_input = Input(shape=inputShape)
+    X = X_input
+    
+    X = Conv2D(64, kernel_size=8,strides=1,padding="same")(X)
+    X = BatchNormalization(axis=3,name="zero_batch_norm")(X)
+    X = Activation("relu")(X)
+   
+    X = Conv2D(128,kernel_size=5,strides=2,padding="valid")(X)
+    X = BatchNormalization(axis=3,name="first_batch_normalization")(X)
+    X = Activation("relu")(X)
+
+    
+    X = Conv2D(256, kernel_size = 4, strides=2, padding="valid")(X)
+    X = BatchNormalization(axis=3, name="second_batch_norm")(X)
+    X = Activation("relu")(X)
+    
+    X = Conv2D(300, kernel_size=3, strides=2, padding="valid")(X)
+    X = BatchNormalization(axis=3,name="third_batch_norm")(X)
+    X = Activation("relu")(X)
+    X = Dropout(0.25)(X)
+    
+    X = ZeroPadding2D((1,1))(X)
+   
+    X = Conv2D(400, kernel_size=3, strides=1, padding="valid")(X)
+    X = BatchNormalization(axis=3,name="fourth_batch_norm")(X)
+    X = Activation("relu")(X)
+    X = Dropout(0.35)(X)
+
+    X = Conv2D(512, kernel_size=4, strides=1, padding="valid")(X)
+    X = BatchNormalization(axis=3,name="fifth_batch_norm")(X)
+    X = Activation("relu")(X)
+    
+
+    flat = Flatten(name="flatten_2")(X)
+    flat = Dropout(0.485)(flat)
+
+
+    flat= Dense(256, activation="relu",kernel_regularizer = regularizers.l2(.0000185))(flat)
+    flat = Dropout(0.485)(flat)
+    X = Dense(128, activation="relu", kernel_regularizer = regularizers.l2(.00002))(flat)
+    X = Dropout(0.5)(X)
+    X = Dense(256,kernel_regularizer = regularizers.l2(.0000185) )(X)#no activation function, took away regularizer so it could scale below activation freely
+    X = Dropout(0.485)(X)
+    X = Activation("relu")(X+flat)    
+    
+    X = Dense(outputDimension, activation ="softmax",kernel_initializer = glorot_uniform(seed=0))(X)
+    model = Model(inputs = X_input, outputs = X, name="myCNN")
+    
+    return model
 
 
 #old model
